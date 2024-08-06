23556ee568133955e85494c8645290dc7cb9c896
kotaroPurple <kotaroPurple@users.noreply.github.com>
2024-08-07 00:30:44 +0900

    Signal Eliminator 追加

---
+++ mathematics/signal_processing/signal_eliminator.py
@@ -0,0 +1,57 @@
+
+"""
+ref: https://www.sciencedirect.com/science/article/pii/S1746809420304328
+"""
+
+import numpy as np
+import scipy.signal as signal
+# import matplotlib.pyplot as plt
+
+
+# Lowpass Filter (8 Hz 以下の低周波ノイズ除去)
+def lowpass_filter(data, cutoff=8, fs=100, order=5):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
+    y = signal.filtfilt(b, a, data)
+    return y
+
+
+# Highpass Filter (17 Hz 以上の高周波ノイズ除去)
+def highpass_filter(data, cutoff=17, fs=100, order=5):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
+    y = signal.filtfilt(b, a, data)
+    return y
+
+
+# 移動平均フィルター
+def moving_average_filter(data, window_size=5):
+    return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
+
+
+# 差分信号の計算
+def calculate_difference_signal(data):
+    return np.diff(data)
+
+
+# Signal Elimination
+def signal_elimination(
+        original_signal, low_cutoff: float, high_cutoff: float, fs: int, window_size: int = 10):
+    # Step 1: 低周波ノイズの除去
+    low_freq_noise = lowpass_filter(original_signal, low_cutoff, fs)
+    signal_without_low_freq_noise = original_signal - low_freq_noise
+
+    # Step 2: 高周波ノイズの除去
+    high_freq_noise = highpass_filter(signal_without_low_freq_noise, high_cutoff, fs)
+    signal_without_high_and_low_freq_noise = signal_without_low_freq_noise - high_freq_noise
+
+    # Step 3: 移動平均フィルターの適用
+    smoothed_signal = moving_average_filter(signal_without_high_and_low_freq_noise, window_size)
+
+    # Step 4: 差分信号の計算
+    difference_signal = calculate_difference_signal(smoothed_signal)
+
+    return smoothed_signal, difference_signal
+
