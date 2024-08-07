2c15705e5befc2401914654b820d15193a05dfcf
kotaroPurple <kotaroPurple@users.noreply.github.com>
2024-08-07 21:14:06 +0900

    fix signal eliminator

--- mathematics/signal_processing/signal_eliminator.py
+++ mathematics/signal_processing/signal_eliminator.py
@@ -9,26 +9,32 @@ import scipy.signal as signal
 
 
 # Lowpass Filter (8 Hz 以下の低周波ノイズ除去)
-def lowpass_filter(data, cutoff=8, fs=100, order=5):
+def lowpass_filter(data, cutoff=8, fs=100, order=5, zero_phase: bool = True):
     nyquist = 0.5 * fs
     normal_cutoff = cutoff / nyquist
     b, a = signal.butter(order, normal_cutoff, btype='low', analog=False)
-    y = signal.filtfilt(b, a, data)
+    if zero_phase:
+        y = signal.filtfilt(b, a, data)
+    else:
+        y = signal.lfilter(b, a, data)
     return y
 
 
 # Highpass Filter (17 Hz 以上の高周波ノイズ除去)
-def highpass_filter(data, cutoff=17, fs=100, order=5):
+def highpass_filter(data, cutoff=17, fs=100, order=5, zero_phase: bool = True):
     nyquist = 0.5 * fs
     normal_cutoff = cutoff / nyquist
     b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
-    y = signal.filtfilt(b, a, data)
+    if zero_phase:
+        y = signal.filtfilt(b, a, data)
+    else:
+        y = signal.lfilter(b, a, data)
     return y
 
 
 # 移動平均フィルター
 def moving_average_filter(data, window_size=5):
-    return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
+    return np.convolve(data, np.ones(window_size)/window_size, mode='same')
 
 
 # 差分信号の計算
@@ -38,20 +44,25 @@ def calculate_difference_signal(data):
 
 # Signal Elimination
 def signal_elimination(
-        original_signal, low_cutoff: float, high_cutoff: float, fs: int, window_size: int = 10):
+        original_signal, low_cutoff: float, high_cutoff: float, fs: int,
+        window_size: int = 10, zero_phase: bool = True):
     # Step 1: 低周波ノイズの除去
-    low_freq_noise = lowpass_filter(original_signal, low_cutoff, fs)
+    low_freq_noise = lowpass_filter(original_signal, low_cutoff, fs, order=5, zero_phase=zero_phase)
     signal_without_low_freq_noise = original_signal - low_freq_noise
 
     # Step 2: 高周波ノイズの除去
-    high_freq_noise = highpass_filter(signal_without_low_freq_noise, high_cutoff, fs)
+    high_freq_noise = highpass_filter(signal_without_low_freq_noise, high_cutoff, fs, order=5, zero_phase=zero_phase)
     signal_without_high_and_low_freq_noise = signal_without_low_freq_noise - high_freq_noise
 
     # Step 3: 移動平均フィルターの適用
     smoothed_signal = moving_average_filter(signal_without_high_and_low_freq_noise, window_size)
+    avg_subtracted = signal_without_high_and_low_freq_noise - smoothed_signal
 
     # Step 4: 差分信号の計算
-    difference_signal = calculate_difference_signal(smoothed_signal)
+    difference_signal = calculate_difference_signal(avg_subtracted)
 
-    return smoothed_signal, difference_signal
+    # Ste; 5: Polarization
+    polarized = np.abs(difference_signal) * difference_signal
+
+    return smoothed_signal, avg_subtracted, difference_signal, polarized
 
