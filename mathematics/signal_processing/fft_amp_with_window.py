4b78f41a7fb4464c249392eeb06bbc387aadfde9
kotaroPurple <kotaroPurple@users.noreply.github.com>
2024-09-06 00:12:08 +0900

    fft 強度が窓関数で変わるか

---
+++ mathematics/signal_processing/fft_amp_with_window.py
@@ -0,0 +1,44 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+# from scipy import signal
+from numpy.typing import NDArray
+
+
+def make_wave(fs: int, time_range: float, freq: float, delta: float) -> tuple[NDArray, NDArray]:
+    length = int(fs * time_range)
+    times = np.arange(length) / fs
+    x = np.sin(2 * np.pi * freq * times - delta)
+    return times, x
+
+
+def get_fft(fs: int, data: NDArray, window: int) -> tuple[NDArray, NDArray]:
+    fft_result = np.fft.rfft(data, window)
+    freqs = np.fft.rfftfreq(window, 1/fs)
+    return fft_result, freqs
+
+
+def main():
+    fs = 500
+    time_range = 10.0
+    freqs = np.arange(1., 70, 0.2)
+    deltas = np.zeros_like(freqs)
+
+    peak_values = []
+
+    for freq, delta in zip(freqs, deltas):
+        _, x = make_wave(fs, time_range, freq, delta)
+        x_with_window = x * np.hanning(len(x))
+        fft_window = 2**(int(np.log2(len(x))))
+        fft_result, fft_freq = get_fft(fs, x_with_window, fft_window)
+        fft_amp = np.abs(fft_result) / len(fft_result)
+        peak_value = np.max(fft_amp)
+        peak_values.append(peak_value)
+
+    #
+    plt.plot(freqs, peak_values)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
