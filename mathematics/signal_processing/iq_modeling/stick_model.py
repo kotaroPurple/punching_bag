ea68abe3b8878f616fb035b683d95a2e54931ea1
kotaroPurple <kotaroPurple@users.noreply.github.com>
2024-10-07 15:34:56 +0900

    quick FuncAnimation

--- mathematics/signal_processing/iq_modeling/stick_model.py
+++ mathematics/signal_processing/iq_modeling/stick_model.py
@@ -10,48 +10,70 @@ LIGHT_OF_SPEED = 3.e8
 SENSOR_FREQUENCY = 24.e9
 ALPHA = 4 * np.pi * SENSOR_FREQUENCY / LIGHT_OF_SPEED
 
+# model
+stick_length = [0.1, 0.05, 0.05, 0.1]
+init_angle = [-np.pi/2, -np.pi/2, np.pi/2, np.pi/2]
+
 
 # アニメーションの設定
 fig1, axes = plt.subplots(ncols=2, nrows=1)
 ax1, ax2 = axes
 
+
 # 最初のフレームを描画する関数
-def init():
+def init_plot():
     ax1.clear()
     ax2.clear()
-    return []
 
+    n_objects = len(stick_length)
 
-# アニメーションの更新関数
-def update(frame, positions, iq_waves, sum_iq_wave):
-    from_ = max(frame - 100, 0)
-    to_ = frame
+    # ax1
+    ax1_objects = [ax1.plot([], [])[0] for _ in range(n_objects)]
 
-    # 1つ目のプロット
-    ax1.clear()
-    ax1.plot(positions[0, from_:to_, 0], positions[0, from_:to_, 1])
-    ax1.plot(positions[1, from_:to_, 0], positions[1, from_:to_, 1])
-    ax1.plot()
+    # ax2
+    ax2_scatter = [ax2.scatter([], [], label=f'IQ {i}', s=10, c=f'C{i}') for i in range(n_objects)]
+    ax2_plot, = ax2.plot([], [], label='Sum IQ Wave', linestyle='--', c='gray')
+    ax2_objects = ax2_scatter + [ax2_plot]
+
+    # 軸の設定
     ax1.set_title('Positions over Time')
     ax1.set_xlabel('X Position')
     ax1.set_ylabel('Y Position')
     ax1.set_xlim(-1., 1.)
     ax1.set_ylim(-0.1, 1.5)
 
-    # 2つ目のプロット
-    ax2.clear()
-    ax2.scatter(np.real(iq_waves[0, frame]), np.imag(iq_waves[0, frame]), label='IQ Wave 1', s=10)
-    ax2.scatter(np.real(iq_waves[1, frame]), np.imag(iq_waves[1, frame]), label='IQ Wave 2', s=10)
-    ax2.plot(
-        sum_iq_wave[from_:to_].real, sum_iq_wave[from_:to_].imag,
-        label='Sum IQ Wave', linestyle='--', c='C2')
     ax2.set_title('IQ Waves')
     ax2.set_xlabel('Real Part')
     ax2.set_ylabel('Imaginary Part')
+    ax2.set_xlim(-4., 4.)
+    ax2.set_ylim(-4., 4.)
     ax2.legend()
-    ax2.set_xlim(-2., 2.)
-    ax2.set_ylim(-2., 2.)
-    return []
+
+    result = ax1_objects + ax2_objects
+    print(len(result), type(result[0]))
+    return result
+
+
+# アニメーションの更新関数
+def update(frame, positions, iq_waves, sum_iq_wave, plt_objects_):
+    from_ = max(frame - 100, 0)
+    to_ = frame
+
+    n_plot = len(plt_objects_)
+    ax1_objects = plt_objects_[:n_plot//2]
+    ax2_objects = plt_objects_[n_plot//2:]
+
+    # 1つ目のプロット
+    for ax1_plot, trajectory in zip(ax1_objects, positions):
+        ax1_plot.set_data(trajectory[from_:to_, 0], trajectory[from_:to_, 1])
+
+    # 2つ目のプロット
+    for ax2_scatter, iq in zip(ax2_objects[:-1], iq_waves):
+        ax2_scatter.set_offsets((iq[frame].real, iq[frame].imag))
+    ax2_plot = ax2_objects[-1]
+    ax2_plot.set_data(sum_iq_wave[from_:to_].real, sum_iq_wave[from_:to_].imag)
+
+    return ax1_objects + ax2_objects
 
 
 def generate_wave(
@@ -69,9 +91,9 @@ def generate_wave(
 def main():
     # define model
     sensor_position = np.zeros(2)
-    stick_center = np.array([0.5, 1.])
-    total_time = 10
-    step_time = 0.002
+    stick_center = np.array([0., 1.])
+    total_time = 3.
+    step_time = 0.001
     times = np.arange(0., total_time, step_time)
     # # angular velocity
     swing_period = 3.
@@ -88,11 +110,9 @@ def main():
             angles[i] = angle_range[1] - coeff * (diff_t - swing_period/2.)
             angular_velocity[i] = -coeff
     # stick
-    length = [0.1, 0.1]
-    init_angle = [-np.pi/2, np.pi/2]
     _positions = []
     _velocities = []
-    for _l, angle, _w in zip(length, init_angle, angular_velocity):
+    for _l, angle, _w in zip(stick_length, init_angle, angular_velocity):
         phi = angles + angle
         tmp = stick_center + _l * np.c_[np.cos(phi), np.sin(phi)]
         tmp_v = _l * _w * np.c_[-np.sin(phi), np.cos(phi)]
@@ -115,8 +135,10 @@ def main():
     sum_iq_wave = np.sum(iq_waves, axis=0)
 
     # アニメーションの生成
-    _ = FuncAnimation(fig1, update, frames=len(times), init_func=init,
-                    fargs=(positions, iq_waves, sum_iq_wave), blit=False, repeat=True)
+    plt_objects = init_plot()
+    _ = FuncAnimation(
+        fig1, update, frames=len(times), init_func=lambda: plt_objects, interval=0.02,
+        fargs=(positions, iq_waves, sum_iq_wave, plt_objects), blit=False, repeat=True)
 
     plt.show()
 
