cbd04f7b81076739c82d57c2469683606173fe6e
kotaroPurple <kotaroPurple@users.noreply.github.com>
2025-05-05 14:13:21 +0900

    update 1D DMD

--- mathematics/signal_processing/dmd/trial_dmd2.py
+++ mathematics/signal_processing/dmd/trial_dmd2.py
@@ -9,7 +9,7 @@ from numpy.typing import NDArray
 
 def make_data(seed: int = 123) -> list[NDArray]:
     np.random.seed(seed)
-    number = 200
+    number = 400
     process_time = 1.0
     t_raw = np.linspace(0, process_time, number)
     trend = 10 * (t_raw - process_time) ** 2
@@ -20,14 +20,18 @@ def make_data(seed: int = 123) -> list[NDArray]:
     return [data, trend, periodic1, periodic2, noise, t_raw]
 
 
-def make_hanakel_matrix(data: NDArray, window_size: int) -> NDArray:
+def make_hankel_matrix(data: NDArray, window_size: int) -> NDArray:
     return sliding_window_view(data, window_size)
 
 
-def flatten_hanakel_matrix(data: NDArray) -> NDArray:
+def flatten_hankel_matrix(data: NDArray) -> NDArray:
     sub1 = data[:-1, 0]
     sub2 = data[-1, :]
     return np.concat((sub1, sub2))
+
+
+def conjugate_transpose(data: NDArray) -> NDArray:
+    return data.conj().T
 
 
 def lower_svd(mat_u: NDArray, sigmas: NDArray, mat_vh: NDArray, rank: int) -> tuple[NDArray, ...]:
@@ -38,9 +42,12 @@ def main():
     data, trend, periodic1, periodic2, noise, times = make_data()
 
     window_size = 70
-    hanakel_mat = make_hanakel_matrix(data, len(data) - window_size)
-    mat_x = hanakel_mat[:, :-1]
-    mat_y = hanakel_mat[:, 1:]
+    hankel_mat = make_hankel_matrix(data, len(data) - window_size)
+    mat_x = hankel_mat[:, :-1]
+    mat_y = hankel_mat[:, 1:]
+
+    print()
+    print(f'{hankel_mat.shape=}, {mat_x.shape=}, {mat_y.shape=}')
 
     # SVD
     _U, _S, _Vh = np.linalg.svd(mat_x)
@@ -48,57 +55,80 @@ def main():
     sigma_array = (_S ** 2) / sigma_sumsq
     cumlative_contrib = (_S ** 2).cumsum() / sigma_sumsq * 100.
 
+    print()
+    print('---- SVD (X) ----')
+    print(f'{_U.shape=}, {_S.shape=}, {_Vh.shape=}')
+
     # lower SVD
     low_rank = 5
     low_u, low_sigmas, low_vh = lower_svd(_U, _S, _Vh, rank=low_rank)
 
+    # lower_x = low_u @ np.diag(low_sigmas) @ low_vh
+    # diff_x = np.abs(mat_x - lower_x)
+
+    # _flatten_x = flatten_hankel_matrix(mat_x)
+    # _flatten_lower_x = flatten_hankel_matrix(lower_x)
+
+    # plt.plot(_flatten_x)
+    # plt.plot(_flatten_lower_x)
+    # plt.show()
+
     # A
-    low_v = low_vh.conj().T
-    mat_a_tilda = low_u.conj().T @ mat_y @ low_v @ np.diag(1. / low_sigmas)
+    low_v = conjugate_transpose(low_vh)
+    low_uh = conjugate_transpose(low_u)
+    inv_low_sigma = np.diag(1. / low_sigmas)
+    mat_a_tilda = low_uh @ mat_y @ low_v @ inv_low_sigma
     eigens, eigen_vectors = np.linalg.eig(mat_a_tilda)
+
+    print()
+    print('---- A_tilda ----')
+    print(f'{mat_a_tilda.shape=}')
 
     # frequency
     dt = times[1] - times[0]
     freqs = np.angle(eigens) / (2 * np.pi *dt)
-    print(freqs)
+    print()
+    print('---- frequency [Hz] ----')
+    print(f'{freqs=}')
 
-    phi = mat_y @ low_v @ np.diag(1. / low_sigmas) @ eigen_vectors
-    vector_b = (phi.conj().T) @ mat_x[:, 0]
-    psi = np.zeros((low_rank, mat_x.shape[1]), dtype=np.complex128)
+    phi = mat_y @ low_v @ inv_low_sigma @ eigen_vectors
+    vector_b = np.linalg.solve(phi.T @ phi, phi.T @ (mat_x[:, 0]))
+
+    print()
+    print('---- Phi, vector b (amplitude) ----')
     print(f'{phi.shape=}')
-    for idx, mu_elem in enumerate(eigens):
-        for _k in range(mat_x.shape[1]):
-            psi[idx, _k] = np.exp(np.log(mu_elem) / dt * _k * dt) * vector_b[idx]
+    print(f'{vector_b.shape=}, {vector_b}')
+
+    # psi = np.zeros((low_rank, mat_x.shape[1]), dtype=np.complex128)
+    # for idx, mu_elem in enumerate(eigens):
+    #     for _k in range(mat_x.shape[1]):
+    #         psi[idx, _k] = np.exp(np.log(mu_elem) / dt * _k * dt) * vector_b[idx]
 
     # Compute DMD reconstruction for each mode
     x_t_list = []
     for idx, mu_elem in enumerate(eigens):
         x_t = []
         for t_ in times:
-            x_t.append(phi[:, idx] * np.exp(np.log(mu_elem) / dt * t_ * dt) * vector_b[idx])
-        x_t = np.array(x_t).T
-        print(x_t.shape)
+            x_t.append(phi[:, idx] * np.exp(np.log(mu_elem) / dt * t_) * vector_b[idx])
+        x_t = np.array(x_t).T  # (L, T)
         x_t_list.append(x_t)
 
     # Convert Hankel matrix to time-series
     x_t_recon = []
     for x_t in x_t_list:
-        # x_t_recon.append(flatten_hanakel_matrix(x_t))
-        x_t_recon.append(x_t[0, :])
+        x_t_recon.append(flatten_hankel_matrix(x_t))
+        # x_t_recon.append(x_t[0, :])
     x_t_recon = np.array(x_t_recon)
     reconstructed = x_t_recon.sum(axis=0)
-    print(reconstructed.shape)
 
-    # plt.scatter(eigens.real, eigens.imag)
-    # plt.xlim(-1.3, 1.3)
-    # plt.ylim(-1.3, 1.3)
-    # plt.show()
+    _, ax = plt.subplots(figsize=(8, 4))
+    for i, one_phi in enumerate(phi.T):
+        ax.plot(one_phi, alpha=0.5, label=str(i))
+    ax.legend()
 
-    # plt.plot(cumlative_contrib)
-    # plt.show()
-
-    plt.plot(times, data)
-    plt.plot(times, reconstructed)
+    _, ax = plt.subplots(figsize=(8, 4))
+    ax.plot(times, data)
+    ax.plot(times, reconstructed[:len(times)])
     plt.show()
 
 
