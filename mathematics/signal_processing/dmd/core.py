f6e7609f31996c0d4b12d773d778eb82b82c5fc3
kotaro <11015194+kotaroPurple@users.noreply.github.com>
2025-05-06 16:42:57 +0900

    update DMD core functions

---
+++ mathematics/signal_processing/dmd/core.py
@@ -0,0 +1,167 @@
+
+import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
+from numpy.typing import NDArray
+
+
+def make_hankel_matrix(data: NDArray, n_rows: int) -> NDArray:
+    """1次元データ列から Hankel Matrix を作る
+
+    Args:
+        data (NDArray): 1次元データ列
+        n_rows (int): 行数
+
+    Returns:
+        NDArray: Hankel Matrix
+    """
+    return sliding_window_view(data, len(data) - n_rows + 1)
+
+
+def flatten_hankel_matrix(hankel_mat: NDArray) -> NDArray:
+    """Hankel Matrix を1次元信号に戻す
+
+    Args:
+        hankel_mat (NDArray): Hankel Matrix
+
+    Returns:
+        NDArray: 1次元信号
+    """
+    sub1 = hankel_mat[:-1, 0]
+    sub2 = hankel_mat[-1, :]
+    return np.concat((sub1, sub2))
+
+
+def conjugate_transpose(mat_data: NDArray) -> NDArray:
+    """随伴行列に変換する
+
+    Args:
+        mat_data (NDArray): 入力行列
+
+    Returns:
+        NDArray: 随伴行列
+    """
+    # 複素共役を取り, 転置する
+    return mat_data.conj().T
+
+
+def apply_svd(mat_data: NDArray) -> tuple[NDArray, NDArray, NDArray]:
+    """SVD を行う。sigma 項は対角成分
+
+    Args:
+        mat_data (NDArray): 入力行列 (p,q)
+
+    Returns:
+        tuple[NDArray, NDArray, NDArray]: U (p,r), sigmas (r), Vh (r,q)
+    """
+    u_mat, sigmas, vh_mat = np.linalg.svd(mat_data, full_matrices=False)
+    return u_mat, sigmas, vh_mat
+
+
+def lower_svd(mat_u: NDArray, sigmas: NDArray, mat_vh: NDArray, rank: int) -> tuple[NDArray, NDArray, NDArray]:
+    """SVD の結果に対して低ランク化する
+
+    Args:
+        mat_u (NDArray): U
+        sigmas (NDArray): 対角成分
+        mat_vh (NDArray): Vh
+        rank (int): 低ランク基準
+
+    Returns:
+        tuple[NDArray, NDArray, NDArray]: U, sigmas, Vh
+    """
+    return mat_u[:, :rank], sigmas[:rank], mat_vh[:rank, :]
+
+
+def apply_standard_dmd(
+        mat_x: NDArray, mat_y: NDArray, svd_u: NDArray, svd_sigmas: NDArray, svd_vh: NDArray,
+        low_rank: int) -> tuple[NDArray, ...]:
+    """Dynamic Mode Decomposition を行う
+
+    Args:
+        mat_x (NDArray): X in (Y=AX)
+        mat_y (NDArray): Y in (Y=AX)
+        svd_u (NDArray): U in X=U.S.Vh
+        svd_sigmas (NDArray): diagonal S in X=U.S.Vh
+        svd_vh (NDArray): Vh in X=U.S.Vh
+        low_rank (int): 低ランク基準
+
+    Returns:
+        tuple[NDArray, ...]: 行列Aの固有値, 行列Aの固有ベクトル, 各モードの振幅
+    """
+    # 低ランク化
+    low_u, low_sigmas, low_vh = lower_svd(svd_u, svd_sigmas, svd_vh, low_rank)
+    # A~
+    low_uh = conjugate_transpose(low_u)
+    low_v = conjugate_transpose(low_vh)
+    inv_sigma = np.diag(1. / low_sigmas)
+    a_tilda = low_uh @ mat_y @ low_v @ inv_sigma
+    # eigen values, eigen vectors
+    eigens, eigen_vectors = np.linalg.eig(a_tilda)
+    # mode
+    mat_phi = mat_y @ low_v @ inv_sigma @ eigen_vectors
+    amps = np.linalg.solve(mat_phi.T @ mat_phi, mat_phi.T @ mat_x[:, 0])
+    return eigens, mat_phi, amps
+
+
+def reconstruct_by_dmd(
+        mat_a_eigens: NDArray, mat_phi: NDArray, amps: NDArray, times: NDArray) -> tuple[NDArray, list[NDArray]]:
+    """DMD結果から波形を再構成する
+
+    Args:
+        mat_a_eigens (NDArray): 行列Aの固有値
+        mat_phi (NDArray): 行列Aの固有ベクトル
+        amps (NDArray): 各モードの振幅
+        times (NDArray): 時刻情報
+
+    Returns:
+        tuple[NDArray, list[NDArray]]: 再構成波形, 各波形の list
+    """
+    # delta time
+    dt = times[1] - times[0]
+    valid_times = times / dt
+    size = len(times)
+    # reconstruct wave
+    wave_list = []
+    for phi, mu, amp in zip(mat_phi.T, mat_a_eigens, amps):
+        xs = phi[:, None] * amp * (np.exp(np.log(mu) * valid_times)[None, :])
+        wave_list.append(flatten_hankel_matrix(xs)[:size])
+    reconstructed = np.sum(np.array(wave_list), axis=0)
+    return reconstructed, wave_list
+
+
+# def make_data(seed: int = 123) -> list[NDArray]:
+#     np.random.seed(seed)
+#     number = 400
+#     process_time = 1.0
+#     t_raw = np.linspace(0, process_time, number)
+#     trend = 10 * (t_raw - process_time) ** 2
+#     periodic1 = np.sin(10 * 2 * np.pi * t_raw) / np.exp(-2 * t_raw)
+#     periodic2 = np.sin(20 * 2 * np.pi * t_raw)
+#     noise = 1.5 * (np.random.rand(number) - 0.5)
+#     data = trend + periodic1 + periodic2 + noise
+#     return [data, trend, periodic1, periodic2, noise, t_raw]
+
+
+# data, trend, periodic1, periodic2, noise, times = make_data()
+
+# window_size = 70
+# hankel_mat = make_hankel_matrix(data, window_size)
+# mat_x = hankel_mat[:, :-1]
+# mat_y = hankel_mat[:, 1:]
+
+# # SVD
+# _U, _S, _Vh = np.linalg.svd(mat_x)
+
+# eigens, mat_phi, amps = apply_standard_dmd(mat_x, mat_y, _U, _S, _Vh, 5)
+# reconstructed, wave_list = reconstruct_by_dmd(eigens, mat_phi, amps, times)
+
+# import matplotlib.pyplot as plt
+
+# plt.plot(times, data)
+# plt.plot(times, reconstructed)
+
+# for one_wave in wave_list:
+#     plt.plot(times, one_wave, alpha=0.5)
+
+# plt.show()
+
--- mathematics/signal_processing/dmd/trial_dmd2.py
+++ mathematics/signal_processing/dmd/trial_dmd2.py
@@ -3,8 +3,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from numpy.lib.stride_tricks import sliding_window_view
 from numpy.typing import NDArray
+
+from core import make_hankel_matrix, apply_svd, apply_standard_dmd, reconstruct_by_dmd
 
 
 def make_data(seed: int = 123) -> list[NDArray]:
@@ -20,151 +21,46 @@ def make_data(seed: int = 123) -> list[NDArray]:
     return [data, trend, periodic1, periodic2, noise, t_raw]
 
 
-def make_hankel_matrix(data: NDArray, window_size: int) -> NDArray:
-    return sliding_window_view(data, window_size)
-
-
-def flatten_hankel_matrix(data: NDArray) -> NDArray:
-    sub1 = data[:-1, 0]
-    sub2 = data[-1, :]
-    return np.concat((sub1, sub2))
-
-
-def conjugate_transpose(data: NDArray) -> NDArray:
-    return data.conj().T
-
-
-def lower_svd(mat_u: NDArray, sigmas: NDArray, mat_vh: NDArray, rank: int) -> tuple[NDArray, ...]:
-    return mat_u[:, :rank], sigmas[:rank], mat_vh[:rank, :]
-
-
-def make_eigen_info(mat: NDArray) -> tuple[NDArray, NDArray]:
-    # eigens are sorted (np.abs(eigens): descending)
-    eigens, eigen_vectors = np.linalg.eig(mat)
-    return eigens, eigen_vectors
-
-
 def main():
     data, trend, periodic1, periodic2, noise, times = make_data()
 
     window_size = 70
-    hankel_mat = make_hankel_matrix(data, len(data) - window_size)
+    hankel_mat = make_hankel_matrix(data, window_size)
     mat_x = hankel_mat[:, :-1]
     mat_y = hankel_mat[:, 1:]
 
-    print()
-    print(f'{hankel_mat.shape=}, {mat_x.shape=}, {mat_y.shape=}')
-
     # SVD
-    _U, _S, _Vh = np.linalg.svd(mat_x)
-    sigma_sumsq = (_S ** 2).sum()
-    sigma_array = (_S ** 2) / sigma_sumsq
-    cumlative_contrib = (_S ** 2).cumsum() / sigma_sumsq * 100.
+    _U, _S, _Vh = apply_svd(mat_x)
+    # sigma_sumsq = (_S ** 2).sum()
+    # sigma_array = (_S ** 2) / sigma_sumsq
+    # cumlative_contrib = (_S ** 2).cumsum() / sigma_sumsq * 100.
 
-    print()
-    print('---- SVD (X) ----')
-    print(f'{_U.shape=}, {_S.shape=}, {_Vh.shape=}')
 
-    # lower SVD
+    # DMD
     low_rank = 5
-    low_u, low_sigmas, low_vh = lower_svd(_U, _S, _Vh, rank=low_rank)
+    eigens, mat_phi, amps = apply_standard_dmd(mat_x, mat_y, _U, _S, _Vh, low_rank)
 
-    # A
-    low_v = conjugate_transpose(low_vh)
-    low_uh = conjugate_transpose(low_u)
-    inv_low_sigma = np.diag(1. / low_sigmas)
-    mat_a_tilda = low_uh @ mat_y @ low_v @ inv_low_sigma
-    eigens, eigen_vectors = np.linalg.eig(mat_a_tilda)
-
-    # predict A
-    predicted_a = low_u @ mat_a_tilda @ low_uh  # (L,L)
-
-    make_eigen_info(predicted_a)
-
-    print()
-    print('---- A_tilda ----')
-    print(f'{mat_a_tilda.shape=}')
-    print(f'{predicted_a.shape=}')
+    # reconstruction
+    reconstructed, wave_list = reconstruct_by_dmd(eigens, mat_phi, amps, times)
 
     # frequency
     dt = times[1] - times[0]
-    freqs = np.angle(eigens) / (2 * np.pi *dt)
+    freqs = np.angle(eigens) / (2 * np.pi * dt)
     print()
     print('---- frequency [Hz] ----')
     print(f'{freqs=}')
 
-    phi = mat_y @ low_v @ inv_low_sigma @ eigen_vectors
-    vector_b = np.linalg.solve(phi.T @ phi, phi.T @ (mat_x[:, 0]))
-
-    print()
-    print('---- Phi, vector b (amplitude) ----')
-    print(f'{phi.shape=}')
-    print(f'{vector_b.shape=}, {vector_b}')
-
-    # psi = np.zeros((low_rank, mat_x.shape[1]), dtype=np.complex128)
-    # for idx, mu_elem in enumerate(eigens):
-    #     for _k in range(mat_x.shape[1]):
-    #         psi[idx, _k] = np.exp(np.log(mu_elem) / dt * _k * dt) * vector_b[idx]
-
-    # Compute DMD reconstruction for each mode
-    x_t_list = []
-    for idx, mu_elem in enumerate(eigens):
-        x_t = []
-        for t_ in times:
-            x_t.append(phi[:, idx] * np.exp(np.log(mu_elem) / dt * t_) * vector_b[idx])
-        x_t = np.array(x_t).T  # (L, T)
-        x_t_list.append(x_t)
-
-    # Convert Hankel matrix to time-series
-    x_t_recon = []
-    for x_t in x_t_list:
-        x_t_recon.append(flatten_hankel_matrix(x_t))
-        # x_t_recon.append(x_t[0, :])
-    x_t_recon = np.array(x_t_recon)
-    reconstructed = x_t_recon.sum(axis=0)
-
     _, ax = plt.subplots(figsize=(8, 4))
-    for i, one_phi in enumerate(phi.T):
-        ax.plot(one_phi.real, alpha=0.5, label=str(i))
+    for i, one_phi in enumerate(mat_phi.T):
+        ax.plot(one_phi.real, alpha=0.5, label=f'freq: {freqs[i]:.2f} [Hz]')
     ax.legend()
 
     _, ax = plt.subplots(figsize=(8, 4))
     ax.plot(times, data)
-    ax.plot(times, reconstructed[:len(times)].real)
+    ax.plot(times, reconstructed)
 
-    for sub_wave in x_t_recon:
-        ax.plot(times, sub_wave[:len(times)].real, alpha=0.5)
-    plt.show()
-
-    # A
-    lowers = 5
-    eigens, eigen_vectors = make_eigen_info(predicted_a)
-    eigens = eigens[:lowers]
-    eigen_vectors = eigen_vectors[:, :lowers]
-    predicted_b = np.linalg.solve(eigen_vectors.T @ eigen_vectors, eigen_vectors.T @ mat_x[:, 0])
-
-    x_t_list = []
-    for idx, mu_elem in enumerate(eigens):
-        x_t = []
-        for t_ in times:
-            x_t.append(eigen_vectors[:, idx] * np.exp(np.log(mu_elem) / dt * t_) * predicted_b[idx])
-        x_t = np.array(x_t).T  # (L, T)
-        x_t_list.append(x_t)
-
-    # Convert Hankel matrix to time-series
-    x_t_recon = []
-    for x_t in x_t_list:
-        x_t_recon.append(flatten_hankel_matrix(x_t))
-        # x_t_recon.append(x_t[0, :])
-    x_t_recon = np.array(x_t_recon)
-    reconstructed = x_t_recon.sum(axis=0)
-
-    _, ax = plt.subplots(figsize=(8, 4))
-    ax.plot(times, data)
-    ax.plot(times, reconstructed[:len(times)].real)
-
-    for sub_wave in x_t_recon:
-        ax.plot(times, sub_wave[:len(times)].real, alpha=0.5)
+    for sub_wave in wave_list:
+        ax.plot(times, sub_wave, alpha=0.5)
     plt.show()
 
 
