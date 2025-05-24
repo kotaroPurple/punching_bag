81e9c23d45afb3481ef42e57aa664712b489b48b
kotaro <11015194+kotaroPurple@users.noreply.github.com>
2025-05-24 16:21:00 +0900

    update online dmd

---
+++ mathematics/signal_processing/dmd/online_dmd.py
@@ -0,0 +1,86 @@
+
+import numpy as np
+from numpy.typing import NDArray
+
+from core import (make_hankel_matrix, apply_svd)
+
+# * initialize
+#   - DMD
+#   - A = Y.(XT) . (X.XT)^(-1)
+#   -   = Q . P
+# * update
+#   - input new data: y_n, x_n
+#   - update Q, P, A
+#   - eigen, eigen vectors from A
+#   - get modes, amplitudes
+
+
+# def _predict_matrix_a(mat_x: NDArray, mat_y: NDArray, low_rank: int) -> NDArray:
+#     svd_u, svd_sigmas, svd_vh = apply_svd(mat_x)
+#     mat_a = predict_matrix_a_by_dmd(mat_y, svd_u, svd_sigmas, svd_vh, low_rank)
+#     return mat_a
+
+
+def _predict_p_and_q_matrix(mat_x: NDArray, mat_y: NDArray) -> tuple[NDArray, NDArray]:
+    # Q = Y.(XT)
+    mat_q = mat_y @ mat_x.T
+    # P = (X.(XT))^(-1)
+    # X.XT = (U.S.Vh).(VhT.ST.UT)
+    #      = U.S.ST.UT
+    # (X.XT)^(-1) = U.(S^(-2)).UT
+    svd_u, svd_sigmas, _ = apply_svd(mat_x)
+    mat_p = svd_u @ np.diag(1. / (svd_sigmas**2)) @ svd_u.T
+    return mat_p, mat_q
+
+
+def _calculate_next_q_matrix(mat_q: NDArray, vector_x: NDArray, vector_y: NDArray) -> NDArray:
+    # Q_next = Q + y.xT
+    return mat_q + np.outer(vector_y, vector_x)
+
+
+def _calculate_next_p_matrix(mat_p: NDArray, vector_x: NDArray):
+    # x: new array
+    # P_next = P - c.P.x.xT.P
+    # c = 1 / (1 + x.T.P.x)
+    c = 1. / (1 + vector_x @ mat_p @ vector_x)
+    next_p = mat_p - c * (mat_p @ np.outer(vector_x, vector_x) @ mat_p)
+    return next_p
+
+
+def _calculate_a_matrix(mat_p: NDArray, mat_q: NDArray) -> NDArray:
+    return mat_q @ mat_p
+
+
+class OnlineDmd:
+    def __init__(self, window_size: int) -> None:
+        self._window_size = window_size
+
+    def set_initial_data(self, data_array: NDArray) -> None:
+        hankel_mat = make_hankel_matrix(data_array, self._window_size)
+        mat_x = hankel_mat[:, :-1]
+        mat_y = hankel_mat[:, 1:]
+        self._mat_p, self._mat_q = _predict_p_and_q_matrix(mat_x, mat_y)
+        self._mat_a = _calculate_a_matrix(self._mat_p, self._mat_q)
+
+        # keep the last col
+        self._last_array = mat_y[:, -1]
+
+    def update(self, new_data: NDArray) -> None:
+        # 過去データの最後を更新する
+        vector_x = self._last_array
+        vector_y = np.r_[self._last_array[1:], new_data]
+        self._mat_q = _calculate_next_q_matrix(self._mat_q, vector_x, vector_y)
+        self._mat_p = _calculate_next_p_matrix(self._mat_p, vector_x)
+        self._mat_a = _calculate_a_matrix(self._mat_p, self._mat_q)
+        # update
+        self._last_array = vector_y
+
+    def reconstruct(self, valid_number: int, time_index: int) -> list[NDArray]:
+        eigens, phi_mat = np.linalg.eig(self._mat_a)
+        bn = np.linalg.solve(phi_mat.T @ phi_mat, phi_mat.T @ self._last_array)
+        wave_list = []
+        # main
+        for i in range(valid_number):
+            xs = phi_mat[0, i] * bn[i] * (1 / (eigens[i] ** np.arange(time_index)[::-1]))
+            wave_list.append(xs)
+        return wave_list
---
+++ mathematics/signal_processing/dmd/trial_simple_online_dmd.py
@@ -0,0 +1,41 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from online_dmd import OnlineDmd
+from util import generate_data
+
+
+def main() -> None:
+    # data
+    data_list = generate_data(mode=0)
+    data = data_list[0]
+    times = data_list[-1]
+    print(f'{data.shape=}')
+
+    # Online DMD
+    window_size = 70
+    remain_size = 10
+    first_size = len(data) - remain_size
+    first_data = data[:first_size]
+    remain_data = data[first_size:]
+
+    dmd = OnlineDmd(window_size)
+    dmd.set_initial_data(first_data)
+
+    for one_data in remain_data:
+        dmd.update(one_data)
+
+    wave_list = dmd.reconstruct(valid_number=6, time_index=len(data))
+    reconstructed = np.sum(np.array(wave_list), axis=0)
+
+    for i, one_wave in enumerate(wave_list):
+        plt.plot(times, one_wave[:len(data)], alpha=0.5, label=f'sub {i}')
+    plt.plot(times, reconstructed[:len(data)], alpha=0.5, c='red', label='reconstruted')
+    plt.plot(times, data, alpha=0.5, c='black', label='original')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
