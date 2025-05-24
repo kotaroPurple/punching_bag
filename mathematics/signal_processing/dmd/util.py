0bb8b77de07b2629934d08d8edda3fac164595a1
kotaro <11015194+kotaroPurple@users.noreply.github.com>
2025-05-24 15:30:19 +0900

    update dmd cores

---
+++ mathematics/signal_processing/dmd/util.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+from numpy.typing import NDArray
+
+
+def generate_data(mode: int) -> list[NDArray]:
+    if mode == 0:
+        return _make_data(123)
+    else:
+        return []
+
+
+def _make_data(seed: int = 123) -> list[NDArray]:
+    np.random.seed(seed)
+    number = 400
+    process_time = 1.0
+    t_raw = np.linspace(0, process_time, number)
+    trend = 10 * (t_raw - process_time) ** 2
+    periodic1 = np.sin(10 * 2 * np.pi * t_raw) / np.exp(-2 * t_raw)
+    periodic2 = np.sin(20 * 2 * np.pi * t_raw)
+    noise = 1.5 * (np.random.rand(number) - 0.5)
+    data = trend + periodic1 + periodic2 + noise
+    return [data, trend, periodic1, periodic2, noise, t_raw]
