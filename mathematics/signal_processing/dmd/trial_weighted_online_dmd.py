03813fd13c1047ac49a389376c8071f87725f47f
kotaro <11015194+kotaroPurple@users.noreply.github.com>
2025-06-30 00:46:35 +0900

    Online DMD 修正中

--- mathematics/signal_processing/dmd/trial_weighted_online_dmd.py
+++ mathematics/signal_processing/dmd/trial_weighted_online_dmd.py
@@ -1,9 +1,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy.typing import NDArray
 
 from weighted_online_dmd import WeightedOnlineDmd
 from util import generate_data
+
+
+def reconstruct(dmd: WeightedOnlineDmd, length: int, threshold: float = 0.98) -> NDArray:
+    wave_list = dmd.reconstruct(valid_number=0, time_index=length, threshold=threshold)
+    reconstructed = np.sum(np.array(wave_list), axis=0)
+    return reconstructed
 
 
 def main() -> None:
@@ -16,42 +23,53 @@ def main() -> None:
         times = data_list[-1]
         window_size = 70
         rho = 0.98
+        n_blocks = 5
     elif mode == 1:
         data = data_list[0][:]
         times = data_list[-1][:]
-        window_size = 100
+        window_size = 40
         rho = 0.98
+        n_blocks = 6
 
     # Online DMD
-    remain_size = len(data) // 2
+    remain_size = int(0.8 * len(data))
     first_size = len(data) - remain_size
     first_data = data[:first_size]
     remain_data = data[first_size:]
 
     dmd = WeightedOnlineDmd(window_size, rho=rho)
+
+    # 1st data
+    reconstructed_list = []
     dmd.set_initial_data(first_data, low_rank_threshold=0.99)
+    _reconstructed = reconstruct(dmd, len(first_data))
+    reconstructed_list.append(_reconstructed)
 
-    for one_data in remain_data:
-        dmd.update(one_data)
+    # for remain data
+    one_block = remain_size // n_blocks
+    for i in range(n_blocks):
+        input_data = remain_data[i * one_block:(i + 1) * one_block]
+        for one_data in input_data:
+            dmd.update(one_data)
+        _reconstructed = reconstruct(dmd, len(input_data))
+        reconstructed_list.append(_reconstructed)
 
-    wave_list = dmd.reconstruct(valid_number=0, time_index=len(data) - window_size + 1, threshold=0.98)
-    reconstructed = np.sum(np.array(wave_list), axis=0)
+    # plot reconstruction
+    count = 0
+    last_time = times[0]
+    last_value = reconstructed_list[0][0]
 
-    for i, one_wave in enumerate(wave_list):
-        plt.plot(times, one_wave[:len(data)], alpha=0.5, label=f'sub {i}')
-    plt.plot(times, reconstructed[:len(data)], alpha=0.5, c='red', label='reconstruted')
-    plt.plot(times, data, alpha=0.5, c='black', label='original')
-    plt.legend()
-    plt.show()
-
-    wave_list = dmd.reconstruct_from_start(
-        start_vec=data[:window_size], valid_number=0, time_index=len(data) - window_size + 1, threshold=0.98)
-    reconstructed = np.sum(np.array(wave_list), axis=0)
-
-    for i, one_wave in enumerate(wave_list):
-        plt.plot(times, one_wave[:len(data)], alpha=0.5, label=f'sub {i}')
-    plt.plot(times, reconstructed[:len(data)], alpha=0.5, c='red', label='reconstruted')
-    plt.plot(times, data, alpha=0.5, c='black', label='original')
+    plt.figure(figsize=(14, 6))
+    for i, one_block in enumerate(reconstructed_list):
+        sub_times = times[count:count + len(one_block)]
+        count += len(one_block)
+        plt.plot(
+            np.r_[last_time, sub_times], np.r_[last_value, one_block[:len(sub_times)]],
+            alpha=0.7, c=f'C{i}', label=f'Block {i}')
+        # for next
+        last_time = sub_times[-1]
+        last_value = one_block[len(sub_times)-1]
+    plt.plot(times[:count], data[:count], alpha=0.5, c='black', label='original')
     plt.legend()
     plt.show()
 
