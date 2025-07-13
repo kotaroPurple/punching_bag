29e81c9d6de5f93ce7c3c6a41e2f96ded9721eda
ko <ko@to.me>
2025-07-13 21:19:55 +0900

    Online DMD モードのプロット追加

--- mathematics/signal_processing/dmd/trial_weighted_online_dmd.py
+++ mathematics/signal_processing/dmd/trial_weighted_online_dmd.py
@@ -7,10 +7,12 @@ from weighted_online_dmd import WeightedOnlineDmd
 from util import generate_data
 
 
-def reconstruct(dmd: WeightedOnlineDmd, length: int, threshold: float = 0.98) -> NDArray:
+def reconstruct(
+        dmd: WeightedOnlineDmd, length: int, threshold: float = 0.98) \
+            -> tuple[list[NDArray], NDArray]:
     wave_list = dmd.reconstruct(valid_number=0, time_index=length, threshold=threshold)
     reconstructed = np.sum(np.array(wave_list), axis=0)
-    return reconstructed
+    return wave_list, reconstructed
 
 
 def main() -> None:
@@ -40,9 +42,11 @@ def main() -> None:
     dmd = WeightedOnlineDmd(window_size, rho=rho)
 
     # 1st data
+    wave_keeper = []
     reconstructed_list = []
     dmd.set_initial_data(first_data, low_rank_threshold=0.99)
-    _reconstructed = reconstruct(dmd, len(first_data))
+    _wave_list, _reconstructed = reconstruct(dmd, len(first_data))
+    wave_keeper.append(_wave_list)
     reconstructed_list.append(_reconstructed)
 
     # for remain data
@@ -51,7 +55,8 @@ def main() -> None:
         input_data = remain_data[i * one_block:(i + 1) * one_block]
         for one_data in input_data:
             dmd.update(float(one_data))
-        _reconstructed = reconstruct(dmd, len(input_data))
+        _wave_list, _reconstructed = reconstruct(dmd, len(input_data))
+        wave_keeper.append(_wave_list)
         reconstructed_list.append(_reconstructed)
 
     # plot reconstruction
@@ -70,6 +75,19 @@ def main() -> None:
         last_time = sub_times[-1]
         last_value = one_block[len(sub_times)-1]
     plt.plot(times[:count], data[:count], alpha=0.5, c='black', label='original')
+    plt.legend()
+    plt.show()
+
+    # plot modes
+    count = 0
+
+    plt.figure(figsize=(14, 6))
+    for i, wave_list in enumerate(wave_keeper):
+        size = len(wave_list[0])
+        sub_times = times[count:count + size]
+        count += size
+        for one_wave in wave_list:
+            plt.plot(sub_times, one_wave[:len(sub_times)], alpha=0.7, c=f'C{i}')
     plt.legend()
     plt.show()
 
